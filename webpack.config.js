89cd3d77312233bfbd318d26334825b7f35f6941
bill wekesa <billwekesa@bills-MacBook-Pro.local>
2019-05-16 15:21:41 +0300

    configure heroku

--- webpack.config.js
+++ webpack.config.js
@@ -7,11 +7,7 @@ const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
 })
 module.exports = {
   entry: './src/index.js',
-  output: {
-    path: path.resolve('dist/'),
-    filename: 'index_bundle.js',
-    publicPath: '/'
-  },
+  mode: "development",
   module: {
     rules: [
       { test: /\.css$/,
